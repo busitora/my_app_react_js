57a4c9cb1d453d638a48c340d5334532c56b6665
busitora <ts12272530@gmail.com>
2021-10-16 08:05:49 +0900

    lint fix

--- src/components/atoms/button/SecondaryButton.jsx
+++ src/components/atoms/button/SecondaryButton.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { BaseButton } from "./BaseButton";
+import { BaseButton } from './BaseButton';
 
 export const SecondaryButton = (props) => {
   const { children } = props;
