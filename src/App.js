01d814e6179b32dc04c3b0c094fb746d11877190
busitora <ts12272530@gmail.com>
2021-10-17 11:09:59 +0900

    layoutのpagesの作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
+import { DefaultLayout } from './components/templates/DefaultLayout';
+
 
 export default function App() {
 
@@ -18,12 +21,14 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user}></UserCard>
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user}></UserCard>
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
