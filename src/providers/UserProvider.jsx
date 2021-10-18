861ae03299bac63a0e3ba5ed6524c8ada4a5371e
busitora <ts12272530@gmail.com>
2021-10-18 09:22:21 +0900

    add comment

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -6,6 +6,8 @@ export const UserProvider = (props) => {
   const { children } = props;
 
   const [userInfo, setUserInfo] = useState(null)
+  // contextが更新されると、関わっているコンポーネントもすべて再レンダリングされる
+
   return (
     <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
